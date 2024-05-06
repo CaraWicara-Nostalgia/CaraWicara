20da2e93d6f892b8e6229047d4a487bc6ea7c246
Karina Aulia Primanti Putri <karinauliap@gmail.com>
2024-05-07 02:14:41 +0700

    feat: add splashscreen

---
+++ app/src/main/java/com/karina/carawicara/ui/screen/SplashScreen.kt
@@ -0,0 +1,63 @@
+package com.karina.carawicara.ui.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.karina.carawicara.R
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SplashScreen(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        contentAlignment = Alignment.TopCenter,
+        modifier = modifier
+            .fillMaxSize() // Mengisi seluruh layar
+            .background(MaterialTheme.colorScheme.primary) // Mengatur background ke warna primary
+    ) {
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(top = 200.dp)
+        ){
+            Text(
+                text = stringResource(id = R.string.welcome),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo",
+            )
+        }
+        Image(
+            painter = painterResource(id = R.drawable.two_child),
+            contentDescription = null,
+            alignment = Alignment.BottomEnd, // Posisi gambar di bawah kanan
+            modifier = Modifier.fillMaxSize() // Mengisi seluruh container
+        )
+    }
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL_4)
+@Composable
+fun SplashScreenPreview() {
+    SplashScreen()
+}
